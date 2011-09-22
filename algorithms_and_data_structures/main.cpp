ccd16571605b861a5ffd803ad9134012745f65f7
zbyhoo <zbyhoo@gmail.com>
2011-09-22 22:05:14 +0200

    initial commit

---
+++ algorithms_and_data_structures/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algorithms_and_data_structures
+//
+//  Created by Zbigniew Kominek on 9/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
